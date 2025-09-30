6cdeaea6c0f54330f6aaa8fa8f55fd09ca34b582
Markus Moen Magnussen <99993823+Bruggoli@users.noreply.github.com>
2025-09-30 23:02:57 +0200

    uncommitted changes, current version isnt working when cloned from github. changes the request slightly

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,34 @@
 var express = require('express');
+var favicon = require('express-favicon');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   try {
-    const defaultUser = "lynx titan";
-    data = await getDataFromApi("https://secure.runescape.com/m=hiscore_oldschool/index_lite.json?player=" + sanitiseInput(defaultUser))
-    if (data !== ""){
-      console.debug(data);
-    } else {
-      console.log("no data to show")
-    }
+    const playerName = req.query.playerName ? req.query.playerName: "Lynx Titan";
 
-    res.render('index', { 
+    data = await getDataFromApi("https://secure.runescape.com/m=hiscore_oldschool/index_lite.json?player=" + sanitiseInput(playerName))
+
+    if (data){
+      res.render('index', { 
       title: 'OSRS stats!', 
       data,
-      playerName: defaultUser,
+      playerName: playerName,
       img: "https://services.runescape.com/m=itemdb_rs/obj_big.gif?id=5520"
     });
+    } else {
+      res.render('index', { 
+      title: 'OSRS stats!', 
+      data: [],
+      playerName: playerName,
+      img: "https://services.runescape.com/m=itemdb_rs/obj_big.gif?id=5520"
+    });
+    }
+
+
   } catch (e) {
     console.error(e);
+    
     res.render('index', {
       title: 'OSRS stats!',
       data: [],
@@ -27,33 +36,6 @@ router.get('/', async function(req, res, next) {
     });
   }
 });
-
-/* get for handling rsn lookup */
-router.get('/search', async function(req, res, next) {
-  try {
-    const playerName = sanitiseInput(req.query.playerName);
-    console.log("Fetching: https://secure.runescape.com/m=hiscore_oldschool/index_lite.json?player=" + playerName);
-    const data = await getDataFromApi("https://secure.runescape.com/m=hiscore_oldschool/index_lite.json?player=" + playerName);
-    console.log(data.skills)
-    if (data && data.length > 0) {
-      res.render('index', {
-        title: 'OSRS stats!',
-        data,
-        playerName: playerName,
-        img: "https://services.runescape.com/m=itemdb_rs/obj_big.gif?id=5520"
-      })
-    } else {
-        res.render('index', { 
-          title: 'OSRS stats!', 
-          data: [], 
-          error: `Player '${playerName}' not found or has no stats`,
-          playerName: playerName
-      });
-    }
-  } catch (e) {
-    console.error("GET error" + e);
-  }
-})
 
 async function getDataFromApi(url) {
   try {
